919b6709d232b8dea7ff53ae8ab05f6bf33602f1
SaraMohsenpour <mohs0025@algonquinlivee.com>
2024-07-14 12:10:15 -0400

    correct some task

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-document.getElementById("pictures").style.columns = "100px 3";
-const items=['Boats', 'Kamnik', 'Candles', 'Portrait', 'Leaves', 'Background', 'Mountains', 'Swans', 'Bridge', 'Maribor', 'Berries', 'Fish']
-const $pictures=document.getElementById('pictures')
-const html=[]
-for(const item of items){
-    html.push(`<p style="text-align: center;">
-    <span style="font-size: 5rem;"></span><br>
-    <img src="images/${item}.png">
-<code>${item}</code>
-</p>`)
-  } 
-$pictures.innerHTML = html.join('')
+const items = ['Boats', 'Kamnik', 'Candles', 'Portrait', 'Leaves', 'Background', 'Mountains', 'Swans', 'Bridge', 'Maribor', 'Berries', 'Fish'];
+const $pictures = document.getElementById('pictures');
+const html = items.map(item => `
+    <p>
+        <img src="images/${item}.png">
+        <code>${item}</code>
+    </p>
+`).join('');
+
+$pictures.innerHTML = html;
